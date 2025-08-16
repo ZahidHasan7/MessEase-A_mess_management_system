45b76b4241828215284d153cd4125ed897276b66
Towhidur Rahman <towhidasif@gmail.com>
2025-08-17 00:27:04 +0600

    App router with pubic and protected routes.

---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Spinner = () => (
+  <div className="flex justify-center items-center p-4">
+    <div className="w-12 h-12 border-4 border-t-[#fe5b56] border-gray-600 rounded-full animate-spin"></div>
+  </div>
+);
+
+export default Spinner;
