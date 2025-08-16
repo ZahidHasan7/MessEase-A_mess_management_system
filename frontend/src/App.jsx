4cb81b5633fa529efe62727a80763c2c7651c2b1
Towhidur Rahman <towhidasif@gmail.com>
2025-08-16 23:37:03 +0600

    Setup main App component structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+// src/App.jsx
+import React from 'react';
+import { AppRouter } from './AppRouter';
+
+export default function App() {
+    return (
+        <div className="bg-gray-900 text-white min-h-screen">
+            <AppRouter />
+        </div>
+    );
+}
